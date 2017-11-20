0ead2dd19d21e36d3111f4232cc4f70c5210e1fa
Tyler Pulse <tyler3321@gmail.com>
2017-11-20 04:14:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,10 +60,10 @@ def all_supplies_in_holidays(holiday_hash)
   puts season.to_s.capitalize + ":"
   holidays.each do |holidays, supplies|
   if holidays == "New years"
-    holiday == "New Years"\
-  end 
-      
-    
+    holiday == "New Years"
+  end
+
+
   a = "  " + holidays.to_s.split("_").join(" ").capitalize!
   b = supplies.join(", ")
   final = a + ": " + b
