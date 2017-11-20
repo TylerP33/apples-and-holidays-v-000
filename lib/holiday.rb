e00f94e928c5fbc91e23505fd5aff8730898a62b
Tyler Pulse <tyler3321@gmail.com>
2017-11-20 02:17:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,9 +62,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    season + holiday
-      end
-  
+    season + holiday = new_array
+    new_array
+  end
+
 
 
 
