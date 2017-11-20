f6fa2b281c0b93f104e28d07fc032b2aa86fdf7d
Tyler Pulse <tyler3321@gmail.com>
2017-11-20 03:46:40 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
   puts season.to_s.capitalize + ":"
   holidays.each do |holidays, supplies|
-  al = holidays.to_json
+  al = holidays.join("")
   puts "  #{al.capitalize}: #{supplies.join(", ")}"
 
     end
