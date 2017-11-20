d413f8e562a7b79790ec629555dfee95b5b382c4
Tyler Pulse <tyler3321@gmail.com>
2017-11-20 04:13:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     end
   a = "  " + holidays.to_s.split("_").join(" ").capitalize!
   b = supplies.join(", ")
-  final = a.capitalize + ": " + b
+  final = a + ": " + b
   puts final
 
     end
